7dc877c33dd7c4eca3aefa105018eaac43dff636
Matt Garnett <matt@mattgarnett.com>
2018-10-17 18:19:13 +0100

    test

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -91,7 +91,7 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx', '.ts', '.tsx'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -122,7 +122,7 @@ module.exports = {
             options: {
               formatter: eslintFormatter,
               eslintPath: require.resolve('eslint'),
-              
+
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -150,7 +150,7 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              
+
               compact: true,
             },
           },
@@ -173,6 +173,7 @@ module.exports = {
           },
           {
             test: /\.css$/,
+            include: [paths.appSrc, paths.appNodeModules],
             loader: ExtractTextPlugin.extract(
               Object.assign(
                 {
